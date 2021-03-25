253290581c0a85b3c396e4d88e84d13139829b7d
pgiraudoux <patrick.giraudoux@univ-fcomte.fr>
2021-03-25 17:10:36 +0100

    update

--- ui.r
+++ ui.r
@@ -4,9 +4,9 @@ fluidPage(
   
   sidebarPanel(
     sliderInput('densren','Densité de prédateurs (ind/km2)', value = 1,min = 0, max = 20, step=0.25),
-    sliderInput('eatAT','Nombre  maximum de campagnols mangés par jour, par prédateur', value = 6, min = 0, max = 200, step=1),
+    sliderInput('eatAT','Nombre  maximum de campagnols mangés par jour, par prédateur', value = 134, min = 0, max = 300, step=1),
     sliderInput('N','Densité initiale de campagnols (ind./ha)', value = 2,   min = 2, max = 1000,step=1),
-    sliderInput('K','K: densité maximale potentielle de campagnols (ind./ha)', value = 500,   min = 10, max = 1000,step=10),
+    sliderInput('K','K: densité maximale potentielle de campagnols (ind./ha)', value = 1000,   min = 10, max = 1000,step=10),
     numericInput('area','Surface surveillée et piégée (ha)',min=1,value=1),
     fileInput('file1', 'Tableau de piégeage (nombre de campagnols piégés sur la surface)', accept=c('text/tab telimited with header', '.txt')),
   ),
